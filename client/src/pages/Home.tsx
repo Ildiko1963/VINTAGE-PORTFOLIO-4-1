1a091b516f7ec77e8dedaed485db382554a41048
jonathan513 <40723834-jonathan513@users.noreply.replit.com>
2025-08-23 22:10:35 +0000

    Restored to '07c5e10dc57ec4c5f19c5a8ffbf95cc2256d6687'
    Replit-Restored-To: 07c5e10dc57ec4c5f19c5a8ffbf95cc2256d6687

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -88,7 +88,6 @@ export default function Home() {
           <HeroSection onNavigate={scrollToSection} />
           
           <AboutSection />
-          <div className="py-8"></div>
           <PortfolioSection />
           <ServicesSection />
           <ContactSection />
